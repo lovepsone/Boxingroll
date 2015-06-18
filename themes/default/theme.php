f33f922847f9cf123667cb42f03a7c63ef666b8d
lovepsone <lovepsone@mail.ru>
2015-06-18 16:50:16 +0300

    [25] fixed html

--- themes/default/theme.php
+++ themes/default/theme.php
@@ -127,7 +127,7 @@
 			{
 				if (defined("GAME_PAGE") && !GAME_PAGE)
 				{
-					echo '<tr><td colspan="2"><span class="boxRightTitle"><b>'.$locale['userStats'].'</b></span></td></tr>';
+					echo '<tr><td colspan="2" width="250px"><span class="boxRightTitle"><b>'.$locale['userStats'].'</b></span></td></tr>';
 					echo '<tr><td colspan="2" align="center"><hr width="90%"></td></tr>';
 					echo '<tr><td class="boxRightText">'.$locale['cSellKey'].'</td><td class="boxRightValue" align="right">'.$USER->SellKey.'</td></tr>';
 					echo '<tr><td class="boxRightText">'.$locale['cOpenChest'].'</td><td class="boxRightValue" align="right">'.$USER->OpenChest.'</td></tr>';
@@ -135,21 +135,21 @@
 	
 					echo '<tr><td colspan="2"><span class="boxRightTitle"><b>'.$locale['keys'].'</b></span></td></tr>';
 					echo '<tr><td colspan="2" align="center"><hr width="90%"></td></tr>';
-					echo '<tr><td class="boxRightText" align="left" valing="middle">';
+					echo '<tr><td colspan="2" class="boxRightText" align="left" valing="middle">';
 					echo '&nbsp;&nbsp;<img src="'.BASEDIR.'images/key/1.png" height="25px"/> x '.$USER->SellKeyNormal;
 					echo '&nbsp;&nbsp;<img src="'.BASEDIR.'images/key/2.png" height="25px"/> x '.$USER->SellKeyGold;
 					echo '&nbsp;&nbsp;<img src="'.BASEDIR.'images/key/3.png" height="25px"/> x '.$USER->SellKeyPlatinum;
 					echo '&nbsp;&nbsp;<img src="'.BASEDIR.'images/key/4.png" height="25px"/> x '.$USER->SellKeyPremium;
-					echo '</td><td class="boxRightValue" align="left"></td></tr>';
+					echo '</td></tr>';
 	
 					echo '<tr><td colspan="2"><span class="boxRightTitle"><b>'.$locale['kopecks'].'</b></span></td></tr>';
 					echo '<tr><td colspan="2" align="center"><hr width="90%"></td></tr>';
-					echo '<tr><td class="boxRightText" align="left" valing="middle">';
+					echo '<tr><td colspan="2" class="boxRightText" align="left" valing="middle">';
 					echo '&nbsp;&nbsp;<img src="'.BASEDIR.'images/kopeck/1.png" height="12px"/> x '.$USER->KopeckNormal;
 					echo '&nbsp;&nbsp;<img src="'.BASEDIR.'images/kopeck/2.png" height="12px"/> x '.$USER->KopeckGold;
 					echo '&nbsp;&nbsp;<img src="'.BASEDIR.'images/kopeck/3.png" height="12x"/> x '.$USER->KopeckPlatinum;
 					echo '&nbsp;&nbsp;<img src="'.BASEDIR.'images/kopeck/4.png" height="12px"/> x '.$USER->KopeckPremium;
-					echo '</td><td class="boxRightValue" align="left"></td></tr>';
+					echo '</td></td></tr>';
 				}
 				else
 				{
